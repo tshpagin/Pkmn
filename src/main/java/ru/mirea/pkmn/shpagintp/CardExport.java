cabfeb5c94164b76bd6c6a1fa1375e6b4771f802
tshpagin <xplayteam00@gmail.com>
2024-10-31 16:00:57 +0300

    Update CardExport.java

--- src/main/java/ru/mirea/pkmn/shpagintp/CardExport.java
+++ src/main/java/ru/mirea/pkmn/shpagintp/CardExport.java
@@ -8,7 +8,7 @@ import java.io.ObjectOutputStream;
 
 public class CardExport {
     public static void Export(Card cardToExport) throws IOException {
-        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\Пользователь\\IdeaProjects\\Pkmn\\src\\main\\resources\\" + cardToExport.getName() + ".crd");
+        FileOutputStream outputStream = new FileOutputStream("src\\main\\resources\\" + cardToExport.getName() + ".crd");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         objectOutputStream.writeObject(cardToExport);
         objectOutputStream.close();
